8ae84c6986de9d77f1a89596e091f5734efcc16a
ky213 <y.keddaoui@gmail.com>
2018-05-29 17:39:20 +0100

    Timer circle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Timer from './components/timer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Timer/>
       </div>
     );
   }
